925c055597b47005ee1edd1a830ad3a50c65f2ba
jml <jean-marc.lienher@bluewin.ch>
2026-08-24 16:04:35 +0200

    i

--- src/defs.h
+++ src/defs.h
@@ -26,55 +26,6 @@
 		(io16.S)*/
 #define MEM_KERNEL_STACK START_ADDRESS
 
-#define XHCI_DEFAULT_BASE0 0xB0000000
-#define XHCI_DEFAULT_BASE1 0xB8000000
-#define XHCI_SUPPORTED_VERSION 0x95
-#define XHCI_HEAP_SIZE (1024 * 1024)
-
-#define XHCI_MAX_CONTROLLER 2
-#define XHCI_MAX_PORT 16
-
-#define PCI_MAX_BUS 256
-#define PCI_MAX_SLOT 32
-#define PCI_MAX_FUNCTION 8
-#define PCI_CONFIG_ADDRESS 0xcf8
-#define PCI_CONFIG_DATA 0xcfc
-
-#define APIC_BASE_MSR 0x1B
-#define APIC_BASE_MSR_BSP 0x100
-#define APIC_BASE_MSR_ENABLE 0x800
-#define APIC_DISABLE 0x10000
-#define APIC_SW_ENABLE 0x100
-#define APIC_CPUFOCUS 0x200
-#define APIC_NMI (4 << 8)
-#define TMR_PERIODIC 0x20000
-#define TMR_BASEDIV (1 << 20)
-
-#define APIC_APICID (0x020 >> 2)
-#define APIC_APICVER (0x030 >> 2)
-#define APIC_TASKPRIOR (0x080 >> 2)
-#define APIC_EOI (0x0B0 >> 2)
-#define APIC_LDR (0x0D0 >> 2)
-#define APIC_DFR (0x0E0 >> 2)
-#define APIC_SPURIOUS (0x0F0 >> 2)
-#define APIC_ESR (0x280 >> 2)
-#define APIC_ICRL (0x300 >> 2)
-#define APIC_ICRH (0x310 >> 2)
-#define APIC_LVT_TMR (0x320 >> 2)
-#define APIC_LVT_PERF (0x340 >> 2)
-#define APIC_LVT_LINT0 (0x350 >> 2)
-#define APIC_LVT_LINT1 (0x360 >> 2)
-#define APIC_LVT_ERR (0x370 >> 2)
-#define APIC_TMRINITCNT (0x380 >> 2)
-#define APIC_LAST (0x38F >> 2)
-#define APIC_TMRCURRCNT (0x390 >> 2)
-#define APIC_TMRDIV (0x3E0 >> 2)
-
-#define IRQ_MASK irq_mask
-#define IRQ_STATUS irq_status
-#define IRQ_COUNTER18 1
-#define IRQ_COUNTER18_NOT 2
-
 #define KERNEL_ENTRY32 (START_ADDRESS + SIZE_16BIT_CODE)
 #define MEM_KERNEL_INFO                                                        \
 	(KERNEL_ENTRY32 + 16) /* kernel info structure full address */
@@ -90,22 +41,5 @@
 #define GDTP_ADDR (KERNEL_ENTRY32 + 0x120)
 #define GDT_ADDR (GDTP_ADDR + 0x40)
 #define KERNEL_ENTRY64 (GDT_ADDR + 0x20 + 0x600)
-
-#define ISR_ADDR (KERNEL_ENTRY64 + 0x800)
-#define ISR_STUB (ISR_ADDR)
-#define ISR_STUB_ERR (ISR_ADDR + 0x10)
-#define ISR_SYSCALL (ISR_ADDR + 0x20)
-#define ISR_TIMER (ISR_ADDR + 0x40)
-#define ISR_KBD (ISR_ADDR + 0x60)
-#define ISR_MOUSE (ISR_ADDR + 0x70)
-#define ISR_USB (ISR_ADDR + 0x80)
-#define ISR_END (ISR_ADDR + 0x100)
-
-#define BEGIN_DESCRIPTORS (ISR_END + 0x400)
-#define SIZE_OF_UEFI                                                           \
-	0x40000 /* FIXME approximation of size of the uefi boot code ()*/
-#define IDT_ADDR ((SIZE_OF_UEFI + BEGIN_DESCRIPTORS + 0xFFF) & ~0xFFF)
-#define PDT_ADDR (IDT_ADDR + 0x8000)
-#define MEM_PROG_END (PDT_ADDR + ((4 * 512 + 4 + 1) * 0x1000))
 
 #endif /* __DEFS_H__ */
